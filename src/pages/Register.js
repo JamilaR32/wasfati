b7b38ef69340f640b272a126c589d4c4c7013e8d
Jamila <j.r.cba@hotmail.com>
2024-01-31 21:31:22 +0300

    registerdesign

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,7 +5,67 @@ import { register } from "../api/auth";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return <div></div>;
+  const [userInfo, setUserInfo] = useState({});
+  const [user, setUser] = useContext(Usercontext);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      setUserInfo({ ...userInfo, [e.target.name]: e.target.files[0] });
+    } else {
+      setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+    }
+  };
+  const { mutate } = useMutation({
+    mutationKey: ["register"],
+    mutationFn: () => register(userInfo),
+    onSuccess: () => {
+      setUser(true);
+      navigate("/");
+    },
+  });
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    mutate();
+  };
+  return (
+    <div className="h-screen w-screen">
+      <div className="flex flex-col justify-center items-center bg-[red]">
+        <div className="flex flex-col">
+          <label>E-mail:</label>
+          <input
+            id="email"
+            name="email"
+            placeholder="write your e-mail"
+            onChange={handleChange}
+          ></input>
+          <label>Username:</label>
+          <input
+            id="username"
+            name="username"
+            placeholder="write your username"
+            onChange={handleChange}
+          ></input>
+          <label>Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="write a unique password"
+            onChange={handleChange}
+          ></input>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmpassword"
+            name="confirmpassword"
+            placeholder="write your password again"
+            onChange={handleChange}
+          ></input>
+          <button>signUp</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
