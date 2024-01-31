1d95b771cc14112024e6508a281d285da8b35500
Jamila <j.r.cba@hotmail.com>
2024-01-31 20:15:47 +0300

    test

---
+++ src/pages/Register.js
@@ -0,0 +1,11 @@
+import React, { useContext, useState } from "react";
+import Usercontext from "../context/Usercontext";
+import { useMutation } from "@tanstack/react-query";
+import { register } from "../api/auth";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  return <div></div>;
+};
+
+export default Register;
